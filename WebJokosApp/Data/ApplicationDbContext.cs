e703cc50de61a8608b8f344e7c419f8fb2535fab
Ascenso3 <7132@aebatalha.ccems.pt>
2023-11-03 16:46:49 +0000

    Adicionar arquivos de projeto.

---
+++ WebJokosApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebJokosApp.Models;
+
+namespace WebJokosApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebJokosApp.Models.Jokes> Jokes { get; set; } = default!;
+    }
+}
---
+++ WebJokosApp/Models/Jokes.cs
@@ -0,0 +1,16 @@
+﻿namespace WebJokosApp.Models
+{
+    public class Jokes
+    {
+        public int ID { get; set; }
+        public string JokesPergunta { get; set;}
+        public string JokesResposta { get; set;}
+
+        public Jokes()
+        {
+        ID = 0;
+        JokesPergunta = string.Empty;
+        JokesResposta = string.Empty;
+        }
+    }
+}
